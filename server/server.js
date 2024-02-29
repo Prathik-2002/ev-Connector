92aab64b73fbdc57d3145eff2bfe1d6fdabcf616
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-29 20:47:16 +0530

    rearranged the code

--- server/server.js
+++ server/server.js
@@ -7,10 +7,22 @@ const {
   disconnectMongoDB,
 } = require('../index');
 
-
 const app = express();
 const PORT = 3000;
 let isDatabaseConnected = false;
+
+app.use(express.json());
+app.use((req, res, next ) => {
+  if (isDatabaseConnected) {
+    next();
+  } else {
+    res.status(503).send('Database not connected');
+  }
+});
+app.use('/Connector', connectorRoutes);
+app.use('/ChargingPoint', chargingPointRoutes);
+app.use('/ChargingStation', chargingStationRoutes);
+
 
 const setIsDatabaseConnected = (connection) => {
   isDatabaseConnected = connection;
@@ -24,17 +36,6 @@ const removeConnection = async () => {
   disconnectMongoDB().then((value) => setIsDatabaseConnected(false));
 };
 
-app.use(express.json());
-app.use((req, res, next ) => {
-  if (isDatabaseConnected) {
-    next();
-  } else {
-    res.status(503).send('Database not connected');
-  }
-});
-app.use('/Connector', connectorRoutes);
-app.use('/ChargingPoint', chargingPointRoutes);
-app.use('/ChargingStation', chargingStationRoutes);
 
 establishConnection('mongodb://localhost/EV2');
 const server = app.listen(PORT);
