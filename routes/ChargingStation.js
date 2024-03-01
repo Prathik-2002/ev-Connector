74caf1812153a86c45bf109d1482d3827c468183
Prathik-2002 <acharyaprathik4@gmail.com>
2024-03-01 12:13:20 +0530

    get stations

--- routes/ChargingStation.js
+++ routes/ChargingStation.js
@@ -6,5 +6,8 @@ chargingStationRoutes.post('/', async (req, res) => {
   const ack = await createNewChargingStation(req.body);
   res.status(201).json(ack);
 });
+chargingStationRoutes.get('/', async (req, res) => {
+  res.status(500).json({message: 'notavailable'});
+});
 module.exports = chargingStationRoutes;
 
